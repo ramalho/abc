67d472eb039099b6c22b875b3bb6962c1c1b6737
Luciano Ramalho <luciano@ramalho.org>
2025-06-12 15:33:47 -0300

    collecting resources

---
+++ poster/ABC-language.mm
@@ -0,0 +1,8 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1328468241887" ID="ID_423541925" MODIFIED="1330259548229" TEXT="ABC language">
+<node CREATED="1328468276783" ID="ID_894185150" MODIFIED="1328468285812" POSITION="right" TEXT="id&#xe9;ias radicais">
+<node CREATED="1328468287278" ID="ID_1238246041" MODIFIED="1328468287278" TEXT=""/>
+</node>
+</node>
+</map>
---
+++ poster/README.md
@@ -0,0 +1,4 @@
+# ABC: the mother of Python
+
+Materials I used to develop a poster about ABC
+presented at PyCon US 2012 in Santa Clara, CA.
